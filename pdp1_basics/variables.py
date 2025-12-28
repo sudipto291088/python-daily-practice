f086896ecefafa982cc13b92ba08ba35f0f1714c
Sudipto Bhadra <60835043+sudipto291088@users.noreply.github.com>
2025-12-28 17:15:59 -0500

    Add files via upload

--- pdp1_basics/variables.py
+++ pdp1_basics/variables.py
@@ -1,3 +1,109 @@
-# Day 01 - Variables
-# Basic variable assignment examples
-# Foundations - Variables, Input, Conditions and Loops
+"""
+Variables – Python Basics
+Covered:
+- assignment
+- data types
+- type()
+"""
+
+
+
+
+
+x = 10
+y = 3
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+
+
+
+
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
+
+
+
+
+
+
+x = 10
+y = 3
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x // y)
+print(x % y)
+
+
+
+
+first_name = "Sudipto"
+last_name = "Bhadra"
+
+full_name = first_name+" "+last_name
+print(full_name)
+
+
+print(full_name.upper())
+print(full_name.lower())
+print(len(full_name))
+
+
+
+
+city = "SilverSpring"
+profession = "IT"
+yoe = 10
+
+print("My name is Sid. "+ " I live in " +city+ "." + " I work as " + profession+"."+" I have {} years of experience".format(yoe))
+
+
+print(
+    f"My name is Sid. I live in {city}."
+    f"I work as {profession}. "
+    f"I have {yoe} years of experience"
+
+    
+)
+
+
+
+
+
+year = 2025
+
+
+print(
+    f"My name is Sid. I live in {city}. "
+    f"I work as {profession}."
+    f"I have {yoe} years of experience"
+    f" as of {year}"
+
+
+    
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+)
+"""
