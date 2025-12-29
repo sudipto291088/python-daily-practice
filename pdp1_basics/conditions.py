2ffe4cdd64fe2b5671a292c9f71dbec7b9d005ce
Sudipto Bhadra <60835043+sudipto291088@users.noreply.github.com>
2025-12-28 19:55:49 -0500

    Add files via upload

---
+++ pdp1_basics/conditions.py
@@ -0,0 +1,48 @@
+'''
+Conditions - Python Basics
+
+Covered: 
+- if/elif/else
+- Comparision Operators
+
+
+'''
+
+
+
+
+## Voting Eligibility
+mage = int(input('Enter your age: '))
+
+if mage>18:
+    print('You can vote! ')
+else:
+    print('Become 18 y first')
+
+
+
+## Grading Eligibility
+score = int(input("Enter your score: "))
+
+
+if score>=90:
+    print("Grade A")
+elif score >=75:
+    print("Grade B")
+elif score >=60:
+    print("Grade C")
+else:
+    print("Too Bad")
+
+
+
+## Experience level
+yoex = int(input("Enter your years of experience: "))
+
+
+if yoex < 3:
+    print('Entry level: ')
+elif 3<yoex<7:
+    print("Mid Level")
+else:
+    print("Senior Level")
