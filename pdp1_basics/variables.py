d5ab114aa39c07147942876d6eeba06748058947
Sudipto Bhadra <60835043+sudipto291088@users.noreply.github.com>
2025-12-28 19:37:10 -0500

    Add files via upload

--- pdp1_basics/variables.py
+++ pdp1_basics/variables.py
@@ -3,39 +3,25 @@ Variables – Python Basics
 Covered:
 - assignment
 - data types
-- type()
+- arithmetic operators
+- strings and f-strings
 """
 
-print("Hello, python1")
-
+# Basic variables
 age = 36
 height = 5.5
 name = "Sid"
 is_student = True
 
+print(age, height, name, is_student)
 
-
-print(age)
-print(height)
-print(name)
-print(is_student)
-
-
-
-
-
-
-
+# Data types
 print(type(age))
 print(type(height))
 print(type(name))
 print(type(is_student))
 
-
-
-
-
-
+# Arithmetic operators
 x = 10
 y = 3
 
@@ -46,52 +32,24 @@ print(x / y)
 print(x // y)
 print(x % y)
 
-
-
-
+# Strings
 first_name = "Sudipto"
 last_name = "Bhadra"
 
-full_name = first_name+" "+last_name
+full_name = first_name + " " + last_name
 print(full_name)
-
-
 print(full_name.upper())
 print(full_name.lower())
 print(len(full_name))
 
-
-
-
+# String formatting
 city = "SilverSpring"
 profession = "IT"
 yoe = 10
-
-print("My name is Sid. "+ " I live in " +city+ "." + " I work as " + profession+"."+" I have {} years of experience".format(yoe))
-
-
-print(
-    f"My name is Sid. I live in {city}."
-    f"I work as {profession}. "
-    f"I have {yoe} years of experience"
-
-    
-)
-
-
-
-
-
 year = 2025
-
 
 print(
     f"My name is Sid. I live in {city}. "
-    f"I work as {profession}."
-    f"I have {yoe} years of experience"
-    f" as of {year}"
-
-
-    
+    f"I work as {profession}. "
+    f"I have {yoe} years of experience as of {year}."
 )
-
