d156c217e06a7ff336bede56985341c420c90fb4
Sudipto Bhadra <60835043+sudipto291088@users.noreply.github.com>
2026-05-12 21:28:32 -0400

    Add files via upload

---
+++ secondlargest/secondlargest.py
@@ -0,0 +1,12 @@
+numbers = [12, 45, 7, 89, 34, 89, 67]
+
+unique_numbers = list(set(numbers))
+unique_numbers.sort()
+
+print("Second largest number:", unique_numbers[-2])
+
+
+
+
+Second largest number: 67
+
