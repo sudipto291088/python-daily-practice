741badb4de95f828ced07cc0e1fe59b2f193aff6
Sudipto Bhadra <60835043+sudipto291088@users.noreply.github.com>
2026-05-17 22:57:37 -0400

    Add files via upload

---
+++ anagram/anagram.py
@@ -0,0 +1,13 @@
+str1 = input("Enter first string: ").lower()
+str2 = input("Enter second string: ").lower()
+
+if sorted(str1) == sorted(str2):
+    print("The strings are anagrams")
+else:
+    print("The strings are not anagrams")
+
+
+
+Enter first string:  race
+Enter second string:  care
+The strings are anagrams
