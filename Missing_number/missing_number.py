f0906314bc52db7ec5556be017e5d4fc71320127
Sudipto Bhadra <60835043+sudipto291088@users.noreply.github.com>
2026-05-18 21:16:04 -0400

    Add files via upload

---
+++ Missing_number/missing_number.py
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3, 5, 6]
+
+n = max(numbers)
+
+expected_sum = n * (n + 1) // 2
+actual_sum = sum(numbers)
+
+missing_number = expected_sum - actual_sum
+
+print("Missing number is:", missing_number)
+
+
+
+
+
+
+Missing number is: 4
+
+
+
