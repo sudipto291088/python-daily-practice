bc8f3b468e08d23d4691d8146421d9069179c554
Sudipto Bhadra <60835043+sudipto291088@users.noreply.github.com>
2026-05-20 23:29:34 -0400

    Add files via upload

---
+++ sort_word_alphabetical_order/sort_word_.py
@@ -0,0 +1,25 @@
+sentence = input("Enter a sentence: ")
+
+words = sentence.split()
+
+words.sort()
+
+print("Words in alphabetical order:")
+
+for word in words:
+    print(word)
+
+
+
+
+
+Enter a sentence:  Hello hope you are having a fine day
+Words in alphabetical order:
+Hello
+a
+are
+day
+fine
+having
+hope
+you
